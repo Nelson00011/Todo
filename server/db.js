93167b436064ff98865be34844707c17ea93ef52
Ayla Nelson <aylanelson01@gmail.com>
2023-10-05 13:16:48 -0700

    ignore TODO update

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "TEST",
+    password: process.env.postgres-pass,
     host: "localhost",
     port: 5432,
     database: "perntodo",
